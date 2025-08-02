1a8e15e9a4870bbcd43a3ff35a05c94ceb9a0907
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2025-08-02 02:34:15 -0400

    Removed unused imports

--- app/src/main/java/com/codepath/campgrounds/CampgroundAdapter.kt
+++ app/src/main/java/com/codepath/campgrounds/CampgroundAdapter.kt
@@ -1,15 +1,10 @@
 package com.codepath.campgrounds
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-
 
 private const val TAG = "CampgroundAdapter"
 
